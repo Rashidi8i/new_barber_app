9eb67cc2705f9206723db7b546782cfd2c9926fc
Rashidi8i <rashidrajpoot298@gmail.com>
2023-06-24 23:05:50 +0500

    gui Updated

--- lib/view/dashboardView/dashboardView.dart
+++ lib/view/dashboardView/dashboardView.dart
@@ -401,7 +401,7 @@ class servicesGrid extends StatelessWidget {
               alignment: Alignment.center,
               decoration: BoxDecoration(
                   color: AppColor.secondaryColor,
-                  borderRadius: BorderRadius.circular(15)),
+                  borderRadius: BorderRadius.circular(9)),
               child: Padding(
                 padding: const EdgeInsets.only(left: 5.0),
                 child: Column(
